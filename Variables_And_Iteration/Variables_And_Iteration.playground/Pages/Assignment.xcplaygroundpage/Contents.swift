3d701e26c8c62ef6c4fac28f9a968b02aa3c34bb
Homann Fritz <fhomann@lcs-mba3118.lcs.on.ca>
2017-10-25 11:59:52 -0400

    got further

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
@@ -56,66 +56,93 @@ let canvas = Canvas(width: 500, height: 500)
 //                    for y in stride(from: 350, to: <#T##Strideable#>, by: <#T##Comparable & SignedNumeric#>)
 
 
-canvas.lineColor = Color.blue
+canvas.lineColor = Color.red
+canvas.defaultLineWidth = 10
 canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
 
 
-canvas.drawLine(fromX: 0, fromY: 500, toX: 50, toY: 450, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 450, toX: 100, toY: 500, lineWidth: 1)
-canvas.drawLine(fromX: 200, fromY: 500, toX: 250, toY: 450, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 450, toX: 300, toY: 500, lineWidth: 1)
-canvas.drawLine(fromX: 400, fromY: 500, toX: 450, toY: 450, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 450, toX: 500, toY: 500, lineWidth: 1)
+//canvas.drawLine(fromX: 0, fromY: 500, toX: 50, toY: 450, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 450, toX: 100, toY: 500, lineWidth: 1)
+//canvas.drawLine(fromX: 200, fromY: 500, toX: 250, toY: 450, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 450, toX: 300, toY: 500, lineWidth: 1)
+//canvas.drawLine(fromX: 400, fromY: 500, toX: 450, toY: 450, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 450, toX: 500, toY: 500, lineWidth: 1)
+// Horizontal change with x
 
 
-canvas.drawLine(fromX: 0, fromY: 450, toX: 50, toY: 400, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 400, toX: 150, toY: 500, lineWidth: 1)
-canvas.drawLine(fromX: 150, fromY: 500, toX: 250, toY: 400, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 400, toX: 350, toY: 500, lineWidth: 1)
-canvas.drawLine(fromX: 350, fromY: 500, toX: 450, toY: 400, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 400, toX: 500, toY: 450, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 400, toX: 0, toY: 350, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 400, toX: 150, toY: 300, lineWidth: 1)
-canvas.drawLine(fromX: 150, fromY: 300, toX: 250, toY: 400, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 400, toX: 350, toY: 300, lineWidth: 1)
-canvas.drawLine(fromX: 350, fromY: 300, toX: 450, toY: 400, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 400, toX: 500, toY: 350, lineWidth: 1)
+for x in stride(from: 0, through: 400, by: 200) {
+
+    
+    // Downward sloping line
+    canvas.drawLine(fromX: x, fromY: 500, toX: x + 50, toY: 450)
+    // Upward sloping line
+    canvas.drawLine(fromX: x + 50, fromY: 450, toX: x + 100, toY: 500)
+
+}
 
 
-canvas.drawLine(fromX: 0, fromY: 300, toX: 50, toY: 350, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 350, toX: 150, toY: 250, lineWidth: 1)
-canvas.drawLine(fromX: 150, fromY: 250, toX: 250, toY: 350, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 350, toX: 350, toY: 250, lineWidth: 1)
-canvas.drawLine(fromX: 350, fromY: 250, toX: 450, toY: 350, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 350, toX: 500, toY: 300, lineWidth: 1)
-canvas.drawLine(fromX: 0, fromY: 200, toX: 50, toY: 150, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 150, toX: 150, toY: 250, lineWidth: 1)
-canvas.drawLine(fromX: 150, fromY: 250, toX: 250, toY: 150, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 150, toX: 350, toY: 250, lineWidth: 1)
-canvas.drawLine(fromX: 350, fromY: 250, toX: 450, toY: 150, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 150, toX: 500, toY: 200, lineWidth: 1)
+//canvas.drawLine(fromX: 0, fromY: 450, toX: 50, toY: 400, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 400, toX: 150, toY: 500, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 500, toX: 250, toY: 400, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 400, toX: 350, toY: 500, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 500, toX: 450, toY: 400, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 400, toX: 500, toY: 450, lineWidth: 1)
+//
+//
+//canvas.drawLine(fromX: 50, fromY: 400, toX: 0, toY: 350, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 400, toX: 150, toY: 300, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 300, toX: 250, toY: 400, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 400, toX: 350, toY: 300, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 300, toX: 450, toY: 400, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 400, toX: 500, toY: 350, lineWidth: 1)
+
+for x in stride(from: 0, through: 400, by: 200) {
+    
+    //upward sloping lines
+    canvas.drawLine(fromX: -50 + x, fromY: 250, toX: x + 50, toY: 350)
+ // downwards sloping lines
+    canvas.drawLine(fromX: x + 50, fromY: 350, toX: x + 50 + 100, toY: 250)
+}
+
+//canvas.drawLine(fromX: 0, fromY: 300, toX: 50, toY: 350, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 350, toX: 150, toY: 250, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 250, toX: 250, toY: 350, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 350, toX: 350, toY: 250, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 250, toX: 450, toY: 350, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 350, toX: 500, toY: 300, lineWidth: 1)
 
 
-canvas.drawLine(fromX: 0, fromY: 150, toX: 50, toY: 100, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 100, toX: 150, toY: 200, lineWidth: 1)
-canvas.drawLine(fromX: 150, fromY: 200, toX: 250, toY: 100, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 100, toX: 350, toY: 200, lineWidth: 1)
-canvas.drawLine(fromX: 350, fromY: 200, toX: 450, toY: 100, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 100, toX: 500, toY: 150, lineWidth: 1)
-canvas.drawLine(fromX: 0, fromY: 50, toX: 50, toY: 100, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 100, toX: 150, toY: 0, lineWidth: 1)
-canvas.drawLine(fromX: 150, fromY: 0, toX: 250, toY: 100, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 100, toX: 350, toY: 0, lineWidth: 1)
-canvas.drawLine(fromX: 350, fromY: 0, toX: 450, toY: 100, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 100, toX: 500, toY: 50, lineWidth: 1)
+for x in stride(from: 0, through: 400, by: 200) {
+
+    //downward sloping lines
+    canvas.drawLine(fromX: -50 + x, fromY: 250, toX: x + 50, toY: 150)
+    //upwards sloping line
+    canvas.drawLine(fromX: x + 50, fromY: 150, toX: x + 150, toY: 250)
+
+}
 
 
-canvas.drawLine(fromX: 0, fromY: 0, toX: 50, toY: 50, lineWidth: 1)
-canvas.drawLine(fromX: 50, fromY: 50, toX: 100, toY: 0, lineWidth: 1)
-canvas.drawLine(fromX: 200, fromY: 0, toX: 250, toY: 50, lineWidth: 1)
-canvas.drawLine(fromX: 250, fromY: 50, toX: 300, toY: 0, lineWidth: 1)
-canvas.drawLine(fromX: 400, fromY: 0, toX: 450, toY: 50, lineWidth: 1)
-canvas.drawLine(fromX: 450, fromY: 50, toX: 500, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 0, fromY: 150, toX: 50, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 100, toX: 150, toY: 200, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 200, toX: 250, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 100, toX: 350, toY: 200, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 200, toX: 450, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 100, toX: 500, toY: 150, lineWidth: 1)
+//
+//canvas.drawLine(fromX: 0, fromY: 50, toX: 50, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 100, toX: 150, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 150, fromY: 0, toX: 250, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 100, toX: 350, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 350, fromY: 0, toX: 450, toY: 100, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 100, toX: 500, toY: 50, lineWidth: 1)
+//
+//
+//canvas.drawLine(fromX: 0, fromY: 0, toX: 50, toY: 50, lineWidth: 1)
+//canvas.drawLine(fromX: 50, fromY: 50, toX: 100, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 200, fromY: 0, toX: 250, toY: 50, lineWidth: 1)
+//canvas.drawLine(fromX: 250, fromY: 50, toX: 300, toY: 0, lineWidth: 1)
+//canvas.drawLine(fromX: 400, fromY: 0, toX: 450, toY: 50, lineWidth: 1)
+//canvas.drawLine(fromX: 450, fromY: 50, toX: 500, toY: 0, lineWidth: 1)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
